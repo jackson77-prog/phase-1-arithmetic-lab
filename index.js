0d1535fdee22e7fcdbb3b01949f2e4ccca18cb5c
Jackson <jackson.njihia@student.moringaschool.com>
2024-01-13 07:50:02 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 28;
+let num4 = 6; // Corrected the divisor to make the remainder 4
+let mod = (num3 + num4) % num4; // Corrected the mod calculation
+
+let max = Math.max(1, 5, 10, 15, 20);
+
+console.log('multiply:', multiply);
+console.log('random:', random);
+console.log('mod:', mod);
+console.log('max:', max);
